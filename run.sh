7c971393e7ed694b0727d51b666036ea903ee2ba
yiyinyinguu <yingluu@zju.edu.cn>
2024-06-11 09:12:22 +0000

    Set up the environment, lack of data

---
+++ run.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+eval "$(conda shell.bash hook)"
+
+conda activate ipcc_ch6f09
+
+DIR=Chapter-6_Fig09
+
+# download the source PNG from IPCC
+# wget https://www.ipcc.ch/report/ar6/wg1/downloads/figures/IPCC_AR6_WGI_Figure_6_9.png
